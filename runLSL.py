db7bafc851fdda04a532353e6994b62d4bd548b9
Yixin Chen <yixincindy19@gmail.com>
2019-01-18 12:15:44 -0800

    fix bug in runLSL script

--- runLSL.py
+++ runLSL.py
@@ -1,27 +1,26 @@
-# This file is to run the corresponding bat and python files
+# This file is to run the corresponding bat, exe and python files
 import subprocess
 import os
 
 # Run the Muse EEG
 current_dir = os.getcwd()
-print("=============",current_dir + "\muse\StreamMuse.bat")
-p1 = subprocess.Popen(current_dir + "\muse\StreamMuse.bat")
-stdout, stderr = p1.communicate()
-print("Muse EEG return code: ", p1.returncode) # return 0 if success
-call(["python", "lsl/outstream_muse.py"])
+#p1 = subprocess.Popen(current_dir + "\muse\StreamMuse.bat")
+#os.system('python lsl/outstream_muse.py')
 
 # Run Polar HR/HVR device
-p2 = Popen("ble/BLEPolarDirect/bin/Debug/BLEPolarDirect.exe")
-stdout, stderr = p2.communicate()
-print("Polar HR/HVR device return code: ", p2.returncode) # return 0 if success
+p2 = subprocess.Popen(current_dir + "\\ble\BLEPolarDirect\\bin\Debug\BLEPolarDirect.exe")
+#stdout, stderr = p2.communicate()
+#print("Polar HR/HVR device return code: ", p2.returncode) # return 0 if success
 
 # Run Shadow Suit applications
 # Make sure having MuseLab under C:\Program Files (x86)\Muse
-p3 = Popen("C:\Program Files\Motion\Shadow.exe")
-stdout, stderr = p3.communicate()
-print("Shadow Suit return code: ", p3.returncode) # return 0 if success
-call(["python", "lsl/outstream_shadow.py"])
+p3 = subprocess.Popen("C:\Program Files\Motion\Shadow.exe")
+#stdout, stderr = p3.communicate()
+#print("Shadow Suit return code: ", p3.returncode) # return 0 if success
+#call(["python", "lsl/outstream_shadow.py"])
+os.system('python lsl/outstream_shadow.py')
 
 # Run Webcam applications
-call(["python", "lsl/outstream_webcam.py"])
+#call(["python", "lsl/outstream_webcam.py"])
+os.system('python lsl/outstream_webcam.py')
 
