78134311ccd2dbe9b5c372c7e951cedba0aa408f
Yixin Chen <yixincindy19@gmail.com>
2019-01-18 12:33:58 -0800

    runLSL.py works
    runLSL basically works, but still need to test in a real scene when connecting to the devices. Also, the output is not very clean when doing a keyboard interrupt. Will look into that next week!

--- runLSL.py
+++ runLSL.py
@@ -4,23 +4,35 @@ import os
 
 # Run the Muse EEG
 current_dir = os.getcwd()
-#p1 = subprocess.Popen(current_dir + "\muse\StreamMuse.bat")
-#os.system('python lsl/outstream_muse.py')
+print("========== Connecting to Muse EEG... ==========")
+p1 = subprocess.Popen(current_dir + "\muse\StreamMuse.bat")
+stdout, stderr = p1.communicate()
+# return 0 if success
+if p1.returncode == 0:
+    print("========== Muse EEG connected! ==========")
+os.system('python lsl/outstream_muse.py')
 
 # Run Polar HR/HVR device
+print("========== Connecting to Polar HR/HVR device... ==========")
 p2 = subprocess.Popen(current_dir + "\\ble\BLEPolarDirect\\bin\Debug\BLEPolarDirect.exe")
-#stdout, stderr = p2.communicate()
-#print("Polar HR/HVR device return code: ", p2.returncode) # return 0 if success
+stdout, stderr = p2.communicate()
+# return 0 if success
+if p2.returncode == 0:
+    print("========== Polar HR/HVR device connected! ==========")
 
 # Run Shadow Suit applications
 # Make sure having MuseLab under C:\Program Files (x86)\Muse
+print("========== Connecting to Shadow Suit... ==========")
 p3 = subprocess.Popen("C:\Program Files\Motion\Shadow.exe")
-#stdout, stderr = p3.communicate()
-#print("Shadow Suit return code: ", p3.returncode) # return 0 if success
-#call(["python", "lsl/outstream_shadow.py"])
-os.system('python lsl/outstream_shadow.py')
+stdout, stderr = p3.communicate()
+# return 0 if success
+if p3.returncode == 0:
+    print("========== Shadow Suit connected! ==========")
+os.system('python lsl/outstream_shadowsuit.py')
 
 # Run Webcam applications
-#call(["python", "lsl/outstream_webcam.py"])
+print("========== Connecting to Webcam... ==========")
 os.system('python lsl/outstream_webcam.py')
+
+print("========== Done! ==========")
 
