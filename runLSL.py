4c95dd8e8026c496c0a740a98d9901b50250ca83
Robert Twomey <robert.twomey@gmail.com>
2019-01-25 11:36:34 -0800

    added some verbose print statements to runLSL

--- runLSL.py
+++ runLSL.py
@@ -1,5 +1,3 @@
-#!/c/Users/Robert/AppData/Local/Programs/Python/Python37-32/python
-
 # This file is to run the corresponding bat, exe and python files
 from subprocess import Popen, PIPE, CREATE_NEW_CONSOLE
 import subprocess
@@ -11,15 +9,22 @@ hrDeviceName = "Polar H7"
 current_dir = os.getcwd()
 
 # Run the Muse EEG
-p1 = subprocess.Popen(current_dir + "\muse\StreamMuse.bat", creationflags=CREATE_NEW_CONSOLE)
-os.system('python lsl/outstream_muse.py')
+# print("=== Opening muse\StreamMuse.bat ===")
+# p1 = subprocess.Popen(current_dir + "\muse\StreamMuse.bat", creationflags=CREATE_NEW_CONSOLE)
 
-# Run Polar HR/HVR device
-p2 = subprocess.Popen(current_dir + "\\ble\BLEPolarDirect\\bin\Debug\BLEPolarDirect.exe" + " \"" + hrDeviceName+"\"")
+# os.system('python lsl/outstream_muse.py')
+# print("=== Opened oustream_muse.py ===")
+
+# # Run Polar HR/HVR device
+# p2 = subprocess.Popen(current_dir + "\\ble\BLEPolarDirect\\bin\Debug\BLEPolarDirect.exe" + " \"" + hrDeviceName+"\"")
+# print("=== Opened BLEPolarDirect ===")
 
 # Run Shadow Suit applications
 # Make sure having MuseLab under C:\Program Files\Motion\Shadow.exe
 p3 = subprocess.Popen("C:\Program Files\Motion\Shadow.exe")
+time.sleep(5.0)
+
+print("=== Opened Motion Shadow Suit ===")
 os.system('python lsl/outstream_shadowsuit.py')
 
 # Run Webcam applications
