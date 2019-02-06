c2a8a477bde084ea66860ae28bd32343fcbf1f11
Robert Twomey <robert.twomey@gmail.com>
2019-02-06 13:24:59 -0800

    added fake mocap data to simulate_outstreams.py

--- lsl/simulate_outstreams.py
+++ lsl/simulate_outstreams.py
@@ -16,22 +16,47 @@ info = StreamInfo('Muse', 'EEG', 8, 100, 'float32', 'myuid34234')
 # next make an outlet
 outlet = StreamOutlet(info)
 
-
+# StreamInfo and Outlet for HRV data
 hrv_info = StreamInfo('Polar', 'HRV', 3, 70, 'float32', 'myuid3')
 
 # next make an outlet
 hrv_outlet = StreamOutlet(hrv_info)
 
+# StreamInfo and Outlet for Shadow Data
+mocap_channels = 32
+mocap_sample_size = 8
+stream_info_mocap = StreamInfo('ShadowSuit', 'MOCAP', mocap_channels * mocap_sample_size, 200)
+channels = stream_info_mocap.desc().append_child("channels")
+	
+channel_list = ["lq0", "lq1", "lq2", "lq3",
+		"c0", "c1", "c2", "c3"]
+
+for c in channel_list:
+	channels.append_child(c) 
+
+# Create outlets
+mocap_outlet = StreamOutlet(stream_info_mocap)
+
 
 print("now sending data...")
 while True:
-    # make a new random 8-channel sample; this is converted into a
-    # pylsl.vectorf (the data type that is expected by push_sample)
-    mysample = [rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand()]
-    # now send it and wait for a bit
-    outlet.push_sample(mysample)
+	# make a new random 8-channel sample; this is converted into a
+	# pylsl.vectorf (the data type that is expected by push_sample)
+	mysample = [rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand()]
+	# now send it and wait for a bit
+	outlet.push_sample(mysample)
 
-    myhrvsample = [rand(), rand(), rand()]
-    hrv_outlet.push_sample(myhrvsample)
+	myhrvsample = [rand(), rand(), rand()]
+	hrv_outlet.push_sample(myhrvsample)
 
-    time.sleep(0.01)
+	# fill mocap sample with random data
+	mymocapsample = []
+
+	for i in range(0,mocap_channels*mocap_sample_size):
+		x = rand()
+		mymocapsample.append(x)
+
+	mocap_outlet.push_sample(mymocapsample)
+	
+	# changes sample rate
+	time.sleep(0.01)
