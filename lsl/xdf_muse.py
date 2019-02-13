47a80961acc65d80b038c99a371eb1cddd29a85b
Yixin Chen <yixincindy19@gmail.com>
2019-02-13 13:04:43 -0800

    save start

---
+++ lsl/xdf_muse.py
@@ -0,0 +1,17 @@
+import os
+import threading
+import socket
+import argparse
+import math
+import time
+import json
+from pythonosc import dispatcher
+from pythonosc import osc_server
+from pylsl import StreamInfo, StreamOutlet
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# Load/Read data from xdf file to where?
+# Setup outlet stream infos
+
+
+# Reflect those data in GUI
